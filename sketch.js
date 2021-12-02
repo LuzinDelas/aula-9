89ee3d55dc8e3bbac00b0cf32c88d899250b1d6c
LuzinDelas <95446605+LuzinDelas@users.noreply.github.com>
2021-12-02 17:57:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+
+
+  var num1= Math.round(random (1,2))
+  if (keyDown("space") && num1 === 1){
+    background("red") 
+  }
+ else if (keyDown("space") && num1 === 2){
+   background("blue")
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  createSprite
+}
+
+
+
+
